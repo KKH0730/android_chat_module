ac521bbb4b73e65d9f5cf1eaeb10b577416d9243
kyungho <kyungho_kim@mycelebs.com>
2022-09-26 12:09:05 +0900

    [refactor] MainViewModel // 네트워크 호출 부분 및 응답 수정

--- app/src/main/java/com/example/base/MainViewModel.kt
+++ app/src/main/java/com/example/base/MainViewModel.kt
@@ -1,15 +1,18 @@
 package com.example.base
 
+import androidx.constraintlayout.compose.Dimension.Companion.value
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.base.domain.GetKeytalksUseCase
+import com.example.base.model.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,15 +23,23 @@ class MainViewModel @Inject constructor(
     private val _keytalkFlow = MutableStateFlow<List<String>?>(null)
     val keytalkFlow = _keytalkFlow.asStateFlow()
 
+    private val _message = MutableSharedFlow<String>()
+    val message = _message.asSharedFlow()
+
     fun getKeytalkList(vertical: String = "kyobo_biz") {
         viewModelScope.launch {
-            val keytalkListResponse = withContext(Dispatchers.IO) {
-                getKeytalksUseCase.execute(vertical)
+            val result = withContext(Dispatchers.IO) {
+                getKeytalksUseCase(vertical)
             }
 
-            Timber.e("kkh keytalkListResponse : $keytalkListResponse")
 
-            _keytalkFlow.value = keytalkListResponse.map { it.value }
+            withContext(Dispatchers.Main) {
+                if (result is Result.Success) {
+                    _keytalkFlow.value = result.data.map { it.value }
+                } else {
+                    _message.emit("network error")
+                }
+            }
         }
     }
 }
